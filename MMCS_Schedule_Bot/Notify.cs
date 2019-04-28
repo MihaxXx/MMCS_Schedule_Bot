03df140bcd4b2fe58fb5bf8ea596d8046e54d001
Mikhail <mikes22@yandex.ru>
2019-04-28 19:08:00 +0300

    Change method of getting curWeek
    Needed to  to simplify changes to the rest of code

--- MMCS_Schedule_Bot/Notify.cs
+++ MMCS_Schedule_Bot/Notify.cs
@@ -47,7 +47,7 @@ namespace Notify
             {
                 logger.Info("Updating curWeek...");
                 logger.Info($"Cur week setting: {TimeOfLesson.curWeek}");
-                Week curWeek = CurrentSubject.GetCurrentWeek();
+                Week curWeek = CurrentSubject.GetCurrentWeekFromAPI();
                 TimeOfLesson.curWeek = curWeek;
                 logger.Info($"Cur week after setting: {TimeOfLesson.curWeek}");
             });
