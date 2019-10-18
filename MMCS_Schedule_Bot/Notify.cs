35d64dd5ac5c9790e7e9f656704b482dd90a5d15
Mikhail <mikes22@yandex.ru>
2019-10-18 22:03:34 +0300

    Fix Cron schedule broken in prev commit

--- MMCS_Schedule_Bot/Notify.cs
+++ MMCS_Schedule_Bot/Notify.cs
@@ -86,7 +86,7 @@ namespace Notify
 
             ITrigger resetNotifiedTrigger = TriggerBuilder.Create()
                 .WithIdentity("resetNotified")
-                .WithSchedule(CronScheduleBuilder.DailyAtHourAndMinute(03, 01))
+                .WithSchedule(CronScheduleBuilder.DailyAtHourAndMinute(03, 02))
                 .ForJob(resetNotified)
                 .Build();
 
@@ -99,7 +99,7 @@ namespace Notify
 
             ITrigger resetWeekTypeTrigger = TriggerBuilder.Create()
                 .WithIdentity("resetWeekType")
-                .WithCronSchedule($"3 0 30 ? * MON *")
+                .WithCronSchedule($"03 01 0 ? * MON *")
                 .ForJob(resetWeekType)
                 .Build();
 
